8a932de62333faab60f054194ee55f4eb40be690
Oceane Villers <ocviller@f1br8s8.clusters.42paris.fr>
2025-07-22 18:34:36 +0200

    target

--- src/push_cost.c
+++ src/push_cost.c
@@ -6,7 +6,7 @@
 /*   By: ocviller <ocviller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 16:32:58 by ocviller          #+#    #+#             */
-/*   Updated: 2025/07/22 17:42:39 by ocviller         ###   ########.fr       */
+/*   Updated: 2025/07/22 18:34:17 by ocviller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,18 @@
 void	set_target(t_stack *node, t_stack **b)
 {
 	t_stack	*b_node;
+	t_stack	*min;
 
 	b_node = *b;
+	min = min_node(*b);
+	node->target = min;
 	while (b_node != NULL)
 	{
-		if (node->value < b_node->value)
+		if (node->value > b_node->value && node->target->value < b_node->value)
 			node->target = b_node;
-		if (node->target)
-			if (node->target->value < b_node->value
-				&& node->value < b_node->value)
-				node->target = b_node;
 		b_node = b_node->next;
 	}
-	if (!node->target)
+	if (node->value < node->target->value)
 		node->target = max_node(*b);
 }
 
